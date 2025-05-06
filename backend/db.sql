cc9201ac8871a8cb2143d97579d3b85742562bcc
ArvindTHEkatare <152494889+ArvindTHEkatare@users.noreply.github.com>
2025-05-06 09:12:09 +0200

    Update db.sql

--- backend/db.sql
+++ backend/db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
-    user_id INT PRIMARY KEY,
-    username VARCHAR(255),
-    email VARCHAR(255)
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE meals_McDonalds (
