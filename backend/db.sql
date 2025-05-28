b2f87b0848e3b3c524e26b0afe7d24314d50dbc8
ArvindTHEkatare <152494889+ArvindTHEkatare@users.noreply.github.com>
2025-05-28 16:23:18 +0200

    Update db.sql

--- backend/db.sql
+++ backend/db.sql
@@ -2,6 +2,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
+    is_admin boolean DEFAULT false
 );
 
 CREATE TABLE meals_mcdonalds (
